15be4879c68605d6df2b9b64bc97999d3c760daf
Devin Jhu <devin.jhu@mths.ca>
2022-05-18 18:25:51 +0000

    code

--- any_number.py
+++ any_number.py
@@ -5,18 +5,68 @@
 # The counter
 
 
+def percentage(level):
+    # this function converts a level to percentage
+
+    # process
+    if level == "4+":
+        percentage_number = 97
+
+    elif level == "4":
+        percentage_number = 91
+
+    elif level == "4-":
+        percentage_number = 83
+
+    elif level == "3+":
+        percentage_number = 78
+
+    elif level == "3":
+        percentage_number = 75
+
+    elif level == "3-":
+        percentage_number = 71
+
+    elif level == "2+":
+        percentage_number = 68
+
+    elif level == "2":
+        percentage_number = 65
+
+    elif level == "2-":
+        percentage_number = 61
+
+    elif level == "1+":
+        percentage_number = 58
+
+    elif level == "1":
+        percentage_number = 55
+
+    elif level == "1-":
+        percentage_number = 51
+
+    elif level == "R":
+        percentage_number = 30
+
+    else:
+        percentage_number = 0
+
+    return percentage_number
+
+
 def main():
-    # this program counts
-    counter = 1000
+    # this function gets input, calls a function, gives output
 
-    # process & output
-    for counter in range(1000, 2001):
-        if counter % 5 == 0:
-            print("\n{0} ".format(counter), end=" ")
-        else:
-            print("{0} ".format(counter), end=" ")
+    # input
+    level = input("Enter level: ")
 
-    counter = counter + 1
+    percentage_number = percentage(level)
+
+    if percentage_number == 0:
+        print("not a percentage")
+    else:
+        print("Level {0} is {1}%.".format(level, percentage_number))
+
     print("\nDone.")
 
 
